924f8786ae3c65b6093939ec3b551c2e3fce1351
veb-bet <73333734+veb-bet@users.noreply.github.com>
2023-04-23 17:08:55 +0300

    Update perfoming_test_task2.py
    add frames with a cat on a black background (so that it looks like an example with output data)

--- perfoming_test_task2.py
+++ perfoming_test_task2.py
@@ -1,3 +1,4 @@
+
 import torch
 import requests
 import numpy as np
@@ -6,11 +7,18 @@ import cv2
 # загружаем модель YOLOv5
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-# задаем размер видео
-frame_width = 1240
-frame_height = 680
+# читаем исходное видео
+cap = cv2.VideoCapture(r'C:\Users\user\Desktop\pythonProject1\videos\1.mp4')
 
-# определяем искомый класс объектов
+# проверяем, удалось ли открыть видео
+if (cap.isOpened() == False):
+    print("Error opening video stream or file")
+
+# получаем размеры исходного видео
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# задаем искомый класс объектов
 class_name = "cat"
 
 # определяем имя выходного файла
@@ -21,23 +29,19 @@ output_file_name2 = f"{class_name}_output.mp4"
 fps = 30
 
 # задаем формат записи видео
-fourcc1 = cv2.VideoWriter_fourcc('M','J','P','G')
+fourcc1 = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 # задаем формат записи видео
-fourcc2 = cv2.VideoWriter_fourcc('H','2','6','4')
-# читаем исходное видео
-cap = cv2.VideoCapture(r'C:\Users\user\Desktop\pythonProject1\videos\1.mp4')
+fourcc2 = cv2.VideoWriter_fourcc('H', '2', '6', '4')
 
-# проверяем, удалось ли открыть видео
-if (cap.isOpened()== False):
-  print("Error opening video stream or file")
+# создаем черное изображение для вставки объектов
+black_image = np.zeros((frame_height, frame_width, 3), dtype="uint8")
 
-# создаем объект для записи выходного видео
+# создаем объекты для записи выходного видео
 out1 = cv2.VideoWriter(output_file_name1, fourcc1, fps, (frame_width, frame_height))
-# создаем объект для записи выходного видео
 out2 = cv2.VideoWriter(output_file_name2, fourcc2, fps, (frame_width, frame_height))
 
 # проходим по всем кадрам видео
-while(cap.isOpened()):
+while (cap.isOpened()):
     # захватываем кадр
     ret, frame = cap.read()
 
@@ -58,9 +62,15 @@ while(cap.isOpened()):
                     if model.names[class_index] == class_name:
                         x1, y1, x2, y2 = det[j, :4].cpu().numpy()
                         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                        object_frame = frame[y1:y2, x1:x2, :]
-                        out1.write(cv2.resize(object_frame, (frame_width, frame_height)))
-                        out2.write(cv2.resize(object_frame, (frame_width, frame_height)))
+
+                        # создаем новое изображение размером с исходное видео и вставляем в него найденный объект в исходном размере
+                        object_frame = black_image.copy()
+                        cv2.rectangle(object_frame, (x1, y1), (x2, y2), (255, 255, 255), -1)
+                        object_frame = cv2.bitwise_and(object_frame, frame)
+
+                        # записываем кадр в выходное видео
+                        out1.write(object_frame)
+                        out2.write(object_frame)
 
         # выводим прогресс выполнения
         current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
